cd880cefe0515dc8ee018292afcfff82c0e368b7
AliceAlfaioli <156820194+AliceAlfaioli@users.noreply.github.com>
2024-02-29 17:47:13 +0100

    Second commit

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
 const imgAll = document.querySelectorAll("img");
+
+// console.log(imgAll);
+
 const cardAll = document.querySelectorAll(".col-md-4");
 const btnHide = document.querySelectorAll(".btn-danger");
 
-console.log(imgAll);
+const allText = document.querySelectorAll(".text-id"); //recuperiamo il testo 9 mins
+
+const form = document.getElementById("form"); //creazione campo di ricerca es.6
+const input = document.getElementById("input");
+
+form.onsubmit = function (e) {
+  e.preventDefault();
+  call(input.value);
+};
 
 btn1.onclick = function () {
   call("cat");
 };
-
 btn2.onclick = function () {
-  call("toscana");
+  call("tuscany");
 };
 
-//////////////////////Funzione fetch
 function call(nome) {
   fetch(`https://api.pexels.com/v1/search?query=[${nome}]`, {
-    method: "GET", // Metodo HTTP get
+    method: "GET",
     headers: {
-      Authorization: "KohqPYN2uv4FBxhaYbOpvqmGAZmBFHpUJz1DYdgOTkoksrTuN4dxAgZt",
+      Authorization: "LsmrJr630MEF76L4MxPiNjr0wtv9aIopdvc6PSi1cDZ4XGSV2dPIcd9Y",
     },
   })
     .then((response) => {
@@ -35,14 +44,24 @@ function call(nome) {
       imgAll.forEach((img, index) => {
         img.setAttribute("src", oggetti.photos[index].src.tiny);
       });
-      /* console.log(oggetti.photos); */
+
+      //For each per sostituire 9 mins nell'id delle foto
+
+      allText.forEach((id, index) => {
+        id.textContent = oggetti.photos[index].id;
+      });
+
+      //   console.log(oggetti.photos);
     })
     .catch((error) => console.log(error));
 }
 
-//////////////////////////funzione che elimina le card
-btnHide.forEach((hide, index) => {
-  hide.onclick = () => {
+//Funzione che elimina le card
+
+btnHide.forEach((btnHide, index) => {
+  //hide,index(secondo valore) parametri a piacimento
+
+  btnHide.onclick = () => {
     cardAll[index].remove();
   };
 });
